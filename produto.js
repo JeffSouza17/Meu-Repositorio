ab1c098756af2ba1195e6383dc11db099c5c29ef
Jefferson Souza <jeffinho.souza17@outlook.com>
2019-10-25 14:27:16 -0300

    Conexão da API com o WEB

--- produto.js
+++ produto.js
@@ -1,31 +1,30 @@
-window.onload = function() {
+window.onload = function () {
     console.warn("OLAAAA MUNDO")
 
-let meuform = $("#formProduto")
+    let meuform = $("#formProduto")
 
-console.log(meuform)
+    console.log(meuform)
 
-meuform.on("submit",  function(event){
-event.preventDefault()
-console.log("clicou")
-prepararBotao()
-})
+    meuform.on("submit", function (event) {
+        event.preventDefault()
+        console.log("clicou")
+        prepararBotao()
+        fetch("http://localhost:8080/storm-api/src/inserirproduto.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "x-www-form-urlencoded;charset=UTF-8"
+            }
+        }).then(function (resposta) {
+            alert(resposta)
+        })
+    })
 
-    function prepararBotao(){
-
-
-
-        let botao =$("button#btnSalvar")
+    function prepararBotao() {
+        let botao = $("button#btnSalvar")
         let ladda = Ladda.create(botao[0]);
         ladda.start()
-
-
-
     }
-function clicou(){
-    prepararBotao()
-}
-
-
-
+    function clicou() {
+        prepararBotao()
+    }
 }
